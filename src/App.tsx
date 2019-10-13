44c117c3609ecba51cdd09dc71da733800c6f889
MattAgn <matthieua@bam.tech>
2019-10-13 17:39:19 +0200

    :heavy_plus_sign: add styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React, { FunctionComponent } from 'react';
 import { SafeAreaView, Text } from 'react-native';
 
+import styled, { ThemeProvider } from './lib/styled-components';
+import { theme } from './styles/theme';
+
 export const App: FunctionComponent = () => {
   return (
-    <SafeAreaView>
-      <Text>WorkoutApp</Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <AppTitle>WorkoutApp</AppTitle>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 };
+
+const AppTitle = styled(Text)`
+  font-size: 24;
+  text-align: center;
+  margin: ${({ theme }): number => theme.margin.x3}px;
+`;
---
+++ src/lib/styled-components.ts
@@ -0,0 +1,20 @@
+import * as styledComponents from 'styled-components/native';
+import { ThemeProps as BasicThemeProps } from 'styled-components';
+
+import { theme } from '../styles/theme';
+import { Override } from '../lib/types';
+
+export interface IThemeProps extends BasicThemeProps<typeof theme> {}
+
+// to prevent the use of 'styled.' instead of 'styled(...)' that creates errors with react-native-web
+type StyledComponents<T extends object> = Override<
+  styledComponents.ReactNativeThemedStyledComponentsModule<T>,
+  { default: styledComponents.ReactNativeStyledInterface<T> }
+>;
+
+const { default: styled, css, ThemeProvider, withTheme } = styledComponents as StyledComponents<
+  typeof theme
+>;
+
+export { css, ThemeProvider, withTheme };
+export default styled;
