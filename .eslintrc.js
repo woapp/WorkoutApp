f1f9215a06efa89671a60616cab3784cf01cbd6a
MattAgn <matthieua@bam.tech>
2019-10-13 11:04:07 +0200

    :wrench: add prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,37 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  plugins: ['eslint-plugin-import', 'react-hooks'],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  rules: {
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    'react/prop-types': 'off',
+    'no-debugger': 'error',
+    'newline-before-return': 'error',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import React, {useEffect} from 'react';
 
-export const App = () => {
+export const App: any = () => {
+  const a = 2;
+
+  useEffect(() => {
+    console.log('coucou', a);
+  }, [a]);
   return (
     <SafeAreaView>
       <Text>WorkoutApp</Text>
