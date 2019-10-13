0b99a7b85dd970c2367f3e477af775f3acdd02fe
MattAgn <matthieua@bam.tech>
2019-10-13 10:25:36 +0200

    :truck: Use typescript template

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>WorkoutApp</Text>
+    </SafeAreaView>
+  );
+};
