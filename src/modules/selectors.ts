a7ae0b6fa559a5f84a9fcac3de1b19de1d21d1fa
MattAgn <matthieua@bam.tech>
2020-02-08 10:50:23 +0100

    :sparkles: (mobx) add selectors

---
+++ src/modules/selectors.ts
@@ -0,0 +1,5 @@
+import { RootStoreType } from './types';
+
+export const exercisesSelector = (store: RootStoreType) => store.exercises;
+export const workoutsSelector = (store: RootStoreType) => store.workouts;
+export const historySelector = (store: RootStoreType) => store.history;
