b7a09888e91e7030d0ca77dc6fdd69d2e6d5e9d7
MattAgn <matthieua@bam.tech>
2020-02-08 15:53:40 +0100

    :sparkles: (workouts) delete workout alert

--- src/components/WorkoutItem/WorkoutItem.tsx
+++ src/components/WorkoutItem/WorkoutItem.tsx
@@ -1,16 +1,30 @@
 import React, { FunctionComponent } from 'react';
 import { useNavigation } from 'react-navigation-hooks';
+import { Alert } from 'react-native';
 
 import styled from '../../utils/styled-components';
 import { WorkoutType } from '../../modules/workout';
 import { Routes } from '../../navigation/routes';
 
-export const WorkoutItem: FunctionComponent<{ workout: WorkoutType }> = ({ workout }) => {
+interface Props {
+  workout: WorkoutType;
+  deleteWorkout: (workout: WorkoutType) => void;
+}
+
+export const WorkoutItem: FunctionComponent<Props> = ({ workout, deleteWorkout }) => {
   const navigation = useNavigation();
+
   const navigateToWorkoutEditor = () => navigation.navigate(Routes.WorkoutEditor, { workout });
 
+  const showDeleteAlert = () => {
+    Alert.alert('', 'Voulez-vous supprimer ce workout ?', [
+      { text: 'Annuler', onPress: () => {} },
+      { text: 'Supprimer', onPress: () => deleteWorkout(workout), style: 'destructive' },
+    ]);
+  };
+
   return (
-    <Container onPress={navigateToWorkoutEditor}>
+    <Container onPress={navigateToWorkoutEditor} onLongPress={showDeleteAlert}>
       <Name>{workout.name}</Name>
       <Exercises>{workout.nbExercises} exercices</Exercises>
     </Container>
