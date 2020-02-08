dd7a2b0ea6ec361f154208d9d371b02241e11faf
MattAgn <matthieua@bam.tech>
2020-02-08 16:35:41 +0100

    :art: (exercises model) make id compulsory

--- src/modules/exercise/model.ts
+++ src/modules/exercise/model.ts
@@ -1,10 +1,9 @@
 import { types } from 'mobx-state-tree';
 
 import { MuscleGroup } from '../types';
-import { generateId } from '../../utils/services/generateId';
 
 export const ExerciseModel = {
   name: types.optional(types.string, 'Exercice'),
   muscleGroups: types.array(types.enumeration<MuscleGroup>(Object.values(MuscleGroup))),
-  id: types.optional(types.identifier, generateId()),
+  id: types.identifier,
 };
