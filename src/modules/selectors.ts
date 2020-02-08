afd9099572b25bad078f0c7345cb5471e34145cb
MattAgn <matthieua@bam.tech>
2020-02-08 11:58:17 +0100

    :sparkles: add root selector

--- src/modules/selectors.ts
+++ src/modules/selectors.ts
@@ -3,3 +3,4 @@ import { RootStoreType } from './types';
 export const exercisesSelector = (store: RootStoreType) => store.exercises;
 export const workoutsSelector = (store: RootStoreType) => store.workouts;
 export const historySelector = (store: RootStoreType) => store.history;
+export const storeSelector = (store: RootStoreType) => store;
