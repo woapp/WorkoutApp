88e21ddd52958808b606137172ea0623b7a590de
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-02-08 12:22:31 +0100

    :sparkles: (WorkoutEditor) add plus button and name input

--- src/pages/WorkoutEditor/WorkoutEditor.tsx
+++ src/pages/WorkoutEditor/WorkoutEditor.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
 
-import { NotYetImplemented } from '../../components/NotYetImplemented';
+import styled from '../../utils/styled-components';
+import { RoundButton } from '../../components/RoundButton';
 
 export const WorkoutEditor: React.FC = () => {
-  return <NotYetImplemented pageTitle="WorkoutEditor" />;
+  const [name, setName] = useState('');
+
+  return (
+    <Container>
+      <NameInput value={name} placeholder="Entraînement" onChangeText={setName} />
+      <RoundButtonContainer>
+        <RoundButton
+          onPress={() => {
+            Alert.alert('add a new exercise');
+          }}
+        />
+      </RoundButtonContainer>
+    </Container>
+  );
 };
+
+const Container = styled.View(props => ({
+  flex: 1,
+  padding: props.theme.margin.x2,
+}));
+
+const NameInput = styled.TextInput(props => ({
+  borderColor: props.theme.colors.lightGrey,
+  borderRadius: 20,
+  borderWidth: 2,
+  padding: props.theme.margin.x1,
+  textAlign: 'center',
+  fontWeight: 'bold',
+  fontSize: 20,
+}));
+
+const RoundButtonContainer = styled.View(props => ({
+  position: 'absolute',
+  bottom: props.theme.margin.x2,
+  right: props.theme.margin.x2,
+}));
