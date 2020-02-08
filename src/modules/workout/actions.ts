ec1c37e477d9dd03747c91f12ef27cc566eb71b9
MattAgn <matthieua@bam.tech>
2020-02-08 11:15:48 +0100

    :rotating_light: (workout modeule) disable lint rule locally

--- src/modules/workout/actions.ts
+++ src/modules/workout/actions.ts
@@ -6,6 +6,8 @@ export const workoutActions = (self: ModelInstanceTypeProps<typeof WorkoutModel>
   setName(newName: string): void {
     self.name = newName;
   },
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   addExecise(): void {},
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   removeExercise(): void {},
 });
