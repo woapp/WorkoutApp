d83d9f35cbd1f94723767d362d3bebb47b52e1d6
MattAgn <matthieua@bam.tech>
2020-02-10 09:50:33 +0100

    :construction: Work in progress

--- src/pages/OngoingWorkoutExercises/OngoingWorkoutExercises.tsx
+++ src/pages/OngoingWorkoutExercises/OngoingWorkoutExercises.tsx
@@ -1,9 +1,47 @@
 import React, { FunctionComponent } from 'react';
+import { SectionList, View, Text } from 'react-native';
+import { observer } from 'mobx-react-lite';
 
-import { NotYetImplemented } from '../../components/NotYetImplemented';
+import { ExerciseSetsType } from '../../modules/exerciseSets';
+import { useStore } from '../../utils/hooks/useStore';
 
 type OngoingWorkoutExercisesProps = {};
 
-export const OngoingWorkoutExercises: FunctionComponent<OngoingWorkoutExercisesProps> = () => {
-  return <NotYetImplemented pageTitle="OngoingWorkoutExercises" />;
-};
+export const OngoingWorkoutExercises: FunctionComponent<OngoingWorkoutExercisesProps> = observer(
+  () => {
+    const { ongoingWorkout } = useStore();
+    console.log('ongoingWorkout.exercises.toJSON()', ongoingWorkout?.exercises.toJSON());
+    console.log('ongoingWorkout.exercises.toJSON().length', ongoingWorkout?.exercises.toJSON().length);
+
+    const formatExercises = () => {
+      if (ongoingWorkout) {
+        return ongoingWorkout.exercises.toJSON().map(exerciseSet => ({
+          title: exerciseSet.exercise.name,
+          data: exerciseSet.sets
+        }))
+      }
+      return []
+    }
+    
+    console.log("formatExercises()",formatExercises())
+
+    const renderWorkoutExercise = ({ item }: { item: ExerciseSetsType }) => (
+      <View style={{ flex: 1, height: 100 }}>
+        <Text>{item.length}</Text>
+      </View>
+    );
+
+
+    return (
+      <View>
+        {ongoingWorkout && (
+          <SectionList
+            keyExtractor={(exerciseSets: ExerciseSetsType) => exerciseSets.id}
+            sections={formatExercises()}
+            renderItem={renderWorkoutExercise}
+          />
+        )}
+      </View>
+    );
+  }
+);
