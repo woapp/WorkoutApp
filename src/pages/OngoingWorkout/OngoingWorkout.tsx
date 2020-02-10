4465577f21576f6269d8b4535ec553cac3bde1f2
MattAgn <matthieua@bam.tech>
2020-02-10 08:06:06 +0100

    :sparkles: (OngoingWorkout) Display ongoing workout exercises

--- src/pages/OngoingWorkout/OngoingWorkout.tsx
+++ src/pages/OngoingWorkout/OngoingWorkout.tsx
@@ -1,16 +1,24 @@
 import React, { FunctionComponent } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { observer } from 'mobx-react-lite';
 
 import styled from '../../utils/styled-components';
 import { useStore } from '../../utils/hooks/useStore';
 
 export const OngoingWorkout: FunctionComponent = observer(() => {
-  const { ongoingWorkout, setOngoingWorkout } = useStore();
+  const { ongoingWorkout } = useStore();
 
   return (
     <View>
-      <Name>{ongoingWorkout.name}</Name>
+      {ongoingWorkout && (
+        <View>
+          <Name>{ongoingWorkout.name}</Name>
+          <Text>EXERCICES</Text>
+          {ongoingWorkout.exercises.map(exerciseSet => (
+            <Text key={exerciseSet.id}>{exerciseSet.exercise.name}</Text>
+          ))}
+        </View>
+      )}
     </View>
   );
 });
