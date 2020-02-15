37d75f5756e275cb407d5f18bd78742c59f3b687
MattAgn <matthieua@bam.tech>
2020-02-15 09:47:44 +0100

    :truck: (History) gather pages in history folder

--- src/navigation/HistoryNavigator.ts
+++ src/navigation/HistoryNavigator.ts
@@ -1,13 +1,13 @@
 import { createStackNavigator } from 'react-navigation-stack';
 
-import { HistoryDetails } from '../pages/HistoryDetails';
-import { History } from '../pages/History';
+import { HistoryDetails } from '../pages/History/HistoryDetails';
+import { HistoryOverview } from '../pages/History/HistoryOverview';
 
 import { Routes } from './routes';
 
 export const HistoryNavigator = createStackNavigator({
   [Routes.History]: {
-    screen: History,
+    screen: HistoryOverview,
     navigationOptions: {
       title: 'Historique',
     },
--- src/pages/History/HistoryOverview/HistoryOverview.tsx
+++ src/pages/History/HistoryOverview/HistoryOverview.tsx
@@ -2,11 +2,11 @@ import React, { FunctionComponent } from 'react';
 import { FlatList } from 'react-native';
 import { observer } from 'mobx-react-lite';
 
-import { WorkoutHistoryItem } from '../../components/WorkoutHistoryItem';
-import { useStore } from '../../utils/hooks/useStore';
-import styled from '../../utils/styled-components';
+import { WorkoutHistoryItem } from '../../../components/WorkoutHistoryItem';
+import { useStore } from '../../../utils/hooks/useStore';
+import styled from '../../../utils/styled-components';
 
-export const History: FunctionComponent = observer(() => {
+export const HistoryOverview: FunctionComponent = observer(() => {
   const { history } = useStore();
 
   const renderWorkoutItem = ({ item }) => <WorkoutHistoryItem workout={item} />;
