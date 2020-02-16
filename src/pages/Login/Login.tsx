0adf2746d99c7cdd6b29e6465723eb811f2b72eb
MattAgn <matthieua@bam.tech>
2020-02-16 17:56:31 +0100

    :wrench: (test) finish setup lint committed+husky

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -7,6 +7,9 @@ import { PrimaryButton } from '@woap/components/PrimaryButton';
 import { TextTitle } from '@woap/components/Texts';
 import { Routes } from '@woap/navigation/routes';
 import { useStore } from '@woap/utils/hooks/useStore';
+
+
+
 import { observer } from 'mobx-react-lite';
 
 export const Login: FunctionComponent<NavigationStackScreenProps> = observer(({ navigation }) => {
