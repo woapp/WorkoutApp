5d3db0df71cc0295584e59d4760008ffa029f16e
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-03-30 22:41:12 +0200

    add icons to top tab bar

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { colors } from '@woap/styles/colors';
+import { Icon } from '@woap/components/Icon';
 
 import { Routes } from './routes';
-import { WorkoutsNavigator } from './WorkoutsNavigator';
 import { HistoryNavigator } from './HistoryNavigator';
 import { HomeNavigator } from './HomeNavigator';
 import { ProfileNavigator } from './ProfileNavigator';
@@ -17,7 +16,7 @@ export type TabNavigatorParamList = {
   [Routes.ProfileNavigator]: undefined;
 };
 
-const ICON_SIZE = 30;
+const ICON_SIZE = 32;
 
 const Tab = createMaterialTopTabNavigator<TabNavigatorParamList>();
 
@@ -26,7 +25,11 @@ export const TabNavigator = () => (
     <Tab.Navigator
       initialRouteName={Routes.HomeNavigator}
       tabBarOptions={{
+        showIcon: true,
         showLabel: false,
+        activeTintColor: colors.green,
+        inactiveTintColor: colors.greyScale[60],
+        iconStyle: { height: ICON_SIZE, width: ICON_SIZE },
         indicatorStyle: { backgroundColor: colors.green },
         style: {
           backgroundColor: colors.background.black,
@@ -34,33 +37,24 @@ export const TabNavigator = () => (
       }}
     >
       <Tab.Screen
-        name={Routes.HomeNavigator}
-        component={HomeNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <Ionicons name="ios-home" size={ICON_SIZE} color={color} />,
-        }}
-      />
-      <Tab.Screen
-        name={Routes.WorkoutsNavigator}
-        component={WorkoutsNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <Ionicons name="ios-fitness" size={ICON_SIZE} color={color} />,
-        }}
-      />
-      <Tab.Screen
         name={Routes.HistoryNavigator}
         component={HistoryNavigator}
         options={{
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="ios-calendar" size={ICON_SIZE} color={color} />
-          ),
+          tabBarIcon: ({ color }) => <Icon name="history" color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name={Routes.HomeNavigator}
+        component={HomeNavigator}
+        options={{
+          tabBarIcon: ({ color }) => <Icon name="home" color={color} />,
         }}
       />
       <Tab.Screen
         name={Routes.ProfileNavigator}
         component={ProfileNavigator}
         options={{
-          tabBarIcon: ({ color }) => <Ionicons name="ios-person" size={ICON_SIZE} color={color} />,
+          tabBarIcon: ({ color }) => <Icon name="profile" color={color} />,
         }}
       />
     </Tab.Navigator>
