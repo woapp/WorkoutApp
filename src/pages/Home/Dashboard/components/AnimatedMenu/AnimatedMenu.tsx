89b591a717a50ad11faaca901fb84dadea025e9f
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-03-31 23:28:45 +0200

    add black veil when menu is open is Dashboard page

--- src/pages/Home/Dashboard/components/AnimatedMenu/AnimatedMenu.tsx
+++ src/pages/Home/Dashboard/components/AnimatedMenu/AnimatedMenu.tsx
@@ -6,6 +6,7 @@ import LinearGradient from 'react-native-linear-gradient';
 import styled from '@woap/utils/styled-components';
 import { colors } from '@woap/styles/colors';
 import { IconName } from '@woap/styles/icons';
+import { BlackVeil } from '@woap/pages/Home/Dashboard/components/BlackVeil';
 
 import { ITEM_HEIGHT, MenuItem } from '../MenuItem/MenuItem';
 
@@ -24,40 +25,45 @@ export const AnimatedMenu: FunctionComponent<Props> = ({ items }) => {
   const [isOpen, setIsOpen] = useState(false);
   const transition = useTransition(isOpen, { duration: 400 });
   const rotate = bInterpolate(transition, 0, Math.PI / 4);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <Container>
-      {items.map((item, index) => {
-        const translateY = bInterpolate(
-          transition,
-          (items.length - index) * (ITEM_HEIGHT + ITEM_MARGIN_BOTTOM),
-          0
-        );
+    <>
+      <BlackVeil shouldHandleOnPressEvents={isOpen} onPress={toggleMenu} opacity={transition} />
+      <Container style={{ overflow: 'visible' }}>
+        {items.map((item, index) => {
+          const translateY = bInterpolate(
+            transition,
+            (items.length - index) * (ITEM_HEIGHT + ITEM_MARGIN_BOTTOM),
+            0
+          );
 
-        return (
-          <ItemContainer
-            style={{
-              opacity: transition,
-              transform: [{ translateY }],
-            }}
-            key={index}
-          >
-            <MenuItem {...item} />
-          </ItemContainer>
-        );
-      })}
-
-      <TouchableWithoutFeedback
-        style={{ ...StyleSheet.absoluteFillObject }}
-        onPress={() => {
-          setIsOpen(!isOpen);
-        }}
-      >
-        <IconContainer style={{ transform: [{ rotate }] }}>
-          <Icon>+</Icon>
-        </IconContainer>
-      </TouchableWithoutFeedback>
-    </Container>
+          return (
+            <ItemContainer
+              style={{
+                opacity: transition,
+                transform: [{ translateY }],
+              }}
+              key={index}
+            >
+              <MenuItem {...item} />
+            </ItemContainer>
+          );
+        })}
+        <TouchableWithoutFeedback
+          style={{ ...StyleSheet.absoluteFillObject }}
+          onPress={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          <IconContainer style={{ transform: [{ rotate }] }}>
+            <Icon>+</Icon>
+          </IconContainer>
+        </TouchableWithoutFeedback>
+      </Container>
+    </>
   );
 };
 
