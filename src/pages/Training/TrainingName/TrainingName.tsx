62d875e429e4aa040435531028eafaebf1598913
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-04-10 19:55:13 +0200

    :sparkles: (Training) add header in training name screen

--- src/pages/Training/TrainingName/TrainingName.tsx
+++ src/pages/Training/TrainingName/TrainingName.tsx
@@ -1,8 +1,22 @@
 import React, { FunctionComponent } from 'react';
-import { NotYetImplemented } from '@woap/components/NotYetImplemented';
+import { Background } from '@woap/components/Background';
+import styled from '@woap/utils/styled-components';
+
+import { Header } from '../components/Header';
 
 interface Props {}
 
 export const TrainingName: FunctionComponent<Props> = () => {
-  return <NotYetImplemented pageTitle="TrainingName" />;
+  return (
+    <Background>
+      <Container>
+        <Header title="New Training" />
+      </Container>
+    </Background>
+  );
 };
+
+const Container = styled.SafeAreaView(({ theme }) => ({
+  margin: theme.margin.x2,
+  flex: 1,
+}));
