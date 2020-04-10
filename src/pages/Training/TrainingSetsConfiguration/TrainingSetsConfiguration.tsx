1a87a017bff4e9e53585404922e572e422abc9ae
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-04-10 19:07:05 +0200

    :sparkles: (Training config) add config for sets

--- src/pages/Training/TrainingSetsConfiguration/TrainingSetsConfiguration.tsx
+++ src/pages/Training/TrainingSetsConfiguration/TrainingSetsConfiguration.tsx
@@ -1,22 +1,62 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
+import DraggableFlatList from 'react-native-draggable-flatlist';
 import LinearGradient from 'react-native-linear-gradient';
 import { CrossIcon } from '@woap/components/Icons/CrossIcon';
 import styled from '@woap/utils/styled-components';
 import { colors } from '@woap/styles/colors';
+import { theme } from '@woap/styles/theme';
 
-export const TrainingSetsConfiguration: FunctionComponent = () => (
-  <Background>
-    <Container>
-      <Header>
-        <Title>Training</Title>
-        <TouchableOpacity onPress={() => {}}>
-          <CrossIcon />
-        </TouchableOpacity>
-      </Header>
-    </Container>
-  </Background>
-);
+import { SetListItem } from './components/SetListItem';
+
+const SETS = [
+  { title: 'CRUNCH', weight: 25, reps: 10, id: '1', selected: false },
+  { title: 'SQUAT', weight: 25, reps: 10, id: '2', selected: false },
+  { title: 'BENCH', weight: 25, reps: 10, id: '3', selected: false },
+];
+
+export const TrainingSetsConfiguration: FunctionComponent = () => {
+  const [sets, setSets] = useState(SETS);
+  const onReorderSets = ({ data }) => {
+    setSets(data);
+  };
+  const renderExercise = ({ item, drag }) => (
+    <SetListItem
+      onDrag={drag}
+      set={item}
+      onPress={() => {
+        console.log('ON PRESS');
+        setSets(prevSets =>
+          prevSets.map(set => ({ ...set, selected: item.id === set.id ? !set.selected : false }))
+        );
+      }}
+    />
+  );
+
+  return (
+    <Background>
+      <Container>
+        <Header>
+          <Title>Training</Title>
+          <TouchableOpacity onPress={() => {}}>
+            <CrossIcon />
+          </TouchableOpacity>
+        </Header>
+        <DraggableFlatList
+          data={sets}
+          renderItem={renderExercise}
+          keyExtractor={item => item.id}
+          onDragEnd={onReorderSets}
+          contentContainerStyle={{
+            paddingTop: theme.margin.x2,
+            paddingHorizontal: theme.margin.x2,
+            paddingBottom: 250,
+          }} // TODO: remove and find find a way for scrollview to be aware of keyboard
+        />
+      </Container>
+    </Background>
+  );
+};
 
 const Background = styled(LinearGradient).attrs({
   start: { x: 0, y: 0 },
@@ -25,12 +65,12 @@ const Background = styled(LinearGradient).attrs({
   flex: 1,
 })``;
 
-const Container = styled.SafeAreaView(({ theme }) => ({
-  margin: theme.margin.x2,
+const Container = styled.SafeAreaView({
+  marginVertical: theme.margin.x2,
   flex: 1,
-}));
+});
 
-const Header = styled.View(({ theme }) => ({
+const Header = styled.View({
   flexDirection: 'row',
   justifyContent: 'space-between',
   alignItems: 'center',
@@ -38,10 +78,11 @@ const Header = styled.View(({ theme }) => ({
   borderBottomColor: theme.colors.transparentWhiteScale[60],
   paddingTop: theme.margin.x1,
   paddingBottom: theme.margin.x2,
-}));
+  marginHorizontal: theme.margin.x2,
+});
 
-const Title = styled.Text(({ theme }) => ({
+const Title = styled.Text({
   ...theme.fonts.h1,
   fontWeight: 'bold',
   color: theme.colors.white,
-}));
+});
