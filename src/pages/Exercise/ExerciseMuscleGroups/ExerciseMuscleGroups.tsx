80368dd0431d6e83f08471f6196c771c0f1dee3b
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-04-11 13:34:50 +0200

    :sparkles: (Exercise) add background and header to muscle groups page

--- src/pages/Exercise/ExerciseMuscleGroups/ExerciseMuscleGroups.tsx
+++ src/pages/Exercise/ExerciseMuscleGroups/ExerciseMuscleGroups.tsx
@@ -1,6 +1,28 @@
 import React, { FunctionComponent } from 'react';
-import { NotYetImplemented } from '@woap/components/NotYetImplemented';
+import { Background } from '@woap/components/Background';
+import { Header } from '@woap/components/Header';
+import { Spacer } from '@woap/components/Spacer';
+import styled from '@woap/utils/styled-components';
 
 export const ExerciseMuscleGroups: FunctionComponent = () => {
-  return <NotYetImplemented pageTitle="ExerciseMuscleGroups" />;
+  return (
+    <Background>
+      <Container>
+        <Header title="New Exercise" />
+        <Spacer height={3} />
+        <Title>CHOOSE AT LEAST ONE MUSCLE GROUP</Title>
+      </Container>
+    </Background>
+  );
 };
+
+const Container = styled.SafeAreaView(({ theme }) => ({
+  margin: theme.margin.x2,
+  flex: 1,
+}));
+
+const Title = styled.Text(({ theme }) => ({
+  ...theme.fonts.h3,
+  color: theme.colors.white,
+  fontWeight: 'bold',
+}));
