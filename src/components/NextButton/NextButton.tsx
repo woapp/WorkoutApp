a49ac2cc523a1323c97c37a6885619367c194af6
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-04-18 22:45:11 +0200

    :sparkles: (NextButton) button is keyboard aware

--- src/components/NextButton/NextButton.tsx
+++ src/components/NextButton/NextButton.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 import styled from '@woap/utils/styled-components';
 
 import { RightArrowIcon } from '../Icons/RightArrowIcon';
@@ -10,23 +11,28 @@ interface Props {
 
 export const NextButton: FunctionComponent<Props> = ({ disabled, onPress }) => {
   return (
-    <Container disabled={disabled} onPress={onPress}>
-      <RightArrowIcon />
-    </Container>
+    <StyledKeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <Container disabled={disabled} onPress={onPress}>
+        <RightArrowIcon />
+      </Container>
+    </StyledKeyboardAvoidingView>
   );
 };
 
 const Container = styled.TouchableOpacity<{ disabled: boolean }>(({ theme, disabled }) => ({
   opacity: disabled ? 0.5 : 1,
-  position: 'absolute',
-  alignSelf: 'flex-end',
-  top: '50%',
-  bottom: '50%',
-  right: 0,
   width: theme.iconSize,
   height: theme.iconSize,
   borderRadius: theme.iconSize / 2,
   alignItems: 'center',
   justifyContent: 'center',
   backgroundColor: theme.colors.black,
+  marginBottom: theme.margin.x3,
 }));
+
+const StyledKeyboardAvoidingView = styled(KeyboardAvoidingView)({
+  position: 'absolute',
+  alignItems: 'flex-end',
+  right: 0,
+  bottom: 0,
+});
