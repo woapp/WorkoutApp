c62e0fb5562a9cfb59d014a78fe070a1f6162772
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-04-24 22:10:13 +0200

    :lipstick: (OngoingTrainingPreview) update style title and favorite

---
+++ src/components/Icons/HeartIcon/HeartIcon.tsx
@@ -0,0 +1,32 @@
+import React, { FunctionComponent } from 'react';
+import Svg, { Path, Defs, LinearGradient, Stop } from 'react-native-svg';
+import { colors } from '@woap/styles/colors';
+
+interface Props {
+  size?: number;
+  selected: boolean;
+}
+
+export const HeartIcon: FunctionComponent<Props> = ({ size = 75, selected }) => {
+  return (
+    <Svg width={size} height={size} viewBox="0 0 75 69" fill="none">
+      <Path
+        d="M73.54 12.647c3.406 10.525.555 21.235-5.453 29.304-3.965 5.472-8.704 10.188-13.55 14.312-4.457 4.149-14.433 12.36-17.077 12.587-2.337-.447-4.96-3.09-6.815-4.45C20.22 56.475 8.995 46.837 3.305 36.3c-4.771-10.117-4.78-22.63 2.646-30.387 9.628-8.68 24.145-6.984 31.509 2.085a21.314 21.314 0 017.296-6.054c11.7-4.67 23.873.089 28.783 10.704z"
+        fill={selected ? 'url(#prefix__paint0_linear)' : colors.greyScale[40]}
+      />
+      <Defs>
+        <LinearGradient
+          id="prefix__paint0_linear"
+          x1={10.156}
+          y1={9.453}
+          x2={60.058}
+          y2={63.931}
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop stopColor="#20CC86" />
+          <Stop offset={1} stopColor="#20CCC2" />
+        </LinearGradient>
+      </Defs>
+    </Svg>
+  );
+};
--- src/pages/Home/OngoingTrainingPreview/OngoingTrainingPreview.tsx
+++ src/pages/Home/OngoingTrainingPreview/OngoingTrainingPreview.tsx
@@ -1,15 +1,15 @@
 import React, { FunctionComponent } from 'react';
-import { TextTitle } from '@woap/components/Texts';
-import styled from '@woap/utils/styled-components';
-import { CompositeNavigationProp, RouteProp } from '@react-navigation/native';
-import { Routes } from '@woap/navigation/routes';
-import { RootNavigatorParamList } from '@woap/navigation';
+import { TouchableOpacity } from 'react-native';
+import { observer } from 'mobx-react-lite';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { MaterialTopTabNavigationProp } from '@react-navigation/material-top-tabs';
+import { CompositeNavigationProp, RouteProp } from '@react-navigation/native';
+import styled from '@woap/utils/styled-components';
+import { Routes } from '@woap/navigation/routes';
+import { RootNavigatorParamList } from '@woap/navigation';
 import { TabNavigatorParamList } from '@woap/navigation/TabNavigator';
 import { HomeNavigatorParamList } from '@woap/navigation/HomeNavigator';
-import { PrimaryButton } from '@woap/components/PrimaryButton';
-import { observer } from 'mobx-react-lite';
+import { HeartIcon } from '@woap/components/Icons/HeartIcon';
 
 type OngoingTrainingPreviewScreenNavigationProp = CompositeNavigationProp<
   StackNavigationProp<RootNavigatorParamList, Routes.TabNavigator>,
@@ -34,16 +34,31 @@ export const OngoingTrainingPreview: FunctionComponent<Props> = observer(({ rout
 
   return (
     <Container>
-      <TextTitle>{training.name}</TextTitle>
-      <PrimaryButton
-        title={training.isFavorite ? 'Delete from favorite' : 'Add to favorite'}
-        // eslint-disable-next-line @typescript-eslint/unbound-method
-        onPress={training.toggleFavorite}
-      />
+      <Row>
+        <TrainingTitle>{training.name}</TrainingTitle>
+        {/* eslint-disable-next-line @typescript-eslint/unbound-method */}
+        <TouchableOpacity onPress={training.toggleFavorite}>
+          <HeartIcon selected={training.isFavorite} size={35} />
+        </TouchableOpacity>
+      </Row>
     </Container>
   );
 });
 
-const Container = styled.View(() => ({
+const Container = styled.View(({ theme }) => ({
+  flex: 1,
+  backgroundColor: theme.colors.background.black,
+}));
+
+const Row = styled.View(({ theme }) => ({
+  flexDirection: 'row',
+  alignItems: 'center',
+  margin: theme.margin.x2,
+}));
+
+const TrainingTitle = styled.Text(({ theme }) => ({
+  ...theme.fonts.h1,
+  color: theme.colors.white,
+  fontWeight: 'bold',
   flex: 1,
 }));
