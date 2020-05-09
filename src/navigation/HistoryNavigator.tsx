d3087c70ddbe89ef62820ee94db643755c7139f1
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-05-09 15:12:45 +0200

    :sparkles: (History) add new screens

--- src/navigation/HistoryNavigator.tsx
+++ src/navigation/HistoryNavigator.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { HistoryDetails } from '@woap/pages/History/HistoryDetails';
 import { HistoryOverview } from '@woap/pages/History/HistoryOverview';
+import { HistoryCalendar } from '@woap/pages/History/HistoryCalendar';
+import { HistoryStatistics } from '@woap/pages/History/HistoryStatistics';
 
 import { Routes } from './routes';
 
 export type HistoryNavigatorParamList = {
   [Routes.HistoryOverview]: undefined;
-  [Routes.HistoryDetails]: {};
+  [Routes.HistoryCalendar]: undefined;
+  [Routes.HistoryStatistics]: undefined;
 };
 
 const Stack = createStackNavigator<HistoryNavigatorParamList>();
 
 export const HistoryNavigator = () => (
-  <Stack.Navigator headerMode="none">
-    <Stack.Screen name={Routes.HistoryOverview} component={HistoryOverview} />
-    <Stack.Screen name={Routes.HistoryDetails} component={HistoryDetails} />
-  </Stack.Navigator>
+  <>
+    <Stack.Navigator headerMode="none" initialRouteName={Routes.HistoryOverview}>
+      <Stack.Screen name={Routes.HistoryOverview} component={HistoryOverview} />
+      <Stack.Screen name={Routes.HistoryCalendar} component={HistoryCalendar} />
+      <Stack.Screen name={Routes.HistoryStatistics} component={HistoryStatistics} />
+    </Stack.Navigator>
+  </>
 );
