69fccd3f3a32fb19d6124a500642c585b1ab9082
MattAgn <matthieua@bam.tech>
2020-05-10 10:26:47 +0200

    :art: (TrainingCompleteAnimation) cleanup styling

--- src/pages/Home/OngoingTraining/components/TrainingCompleteAnimation/TrainingCompleteAnimation.tsx
+++ src/pages/Home/OngoingTraining/components/TrainingCompleteAnimation/TrainingCompleteAnimation.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, useRef, useEffect, useState } from 'react';
 import LottieView from 'lottie-react-native';
-import { View, Text, Animated, Easing } from 'react-native';
+import { Animated, Easing } from 'react-native';
+import styled from '@woap/utils/styled-components';
 
 interface Props {}
 
@@ -17,53 +18,48 @@ export const TrainingCompleteAnimation: FunctionComponent<Props> = () => {
   }, [progress]);
 
   return (
-    <View
-      style={{
-        position: 'absolute',
-        alignItems: 'center',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        top: 0,
-        height: '100%',
-        width: '100%',
-        zIndex: 1,
-      }}
-    >
-      <LottieView
+    <Container>
+      <BlackVeil />
+      <CheckAnimation
         source={require('./trainingCompleteAnimation.json')}
         ref={animationRef}
         loop={false}
         resizeMode="contain"
         progress={progress}
-        style={{ opacity: 1, zIndex: 2, flex: 1 }}
       />
-      <View
-        style={{
-          position: 'absolute',
-          bottom: 0,
-          left: 0,
-          right: 0,
-          top: 0,
-          height: '120%',
-          width: '100%',
-          backgroundColor: 'black',
-          opacity: 0.85,
-          zIndex: 1,
-        }}
-      />
-      <Text
-        style={{
-          color: 'white',
-          position: 'absolute',
-          bottom: '15%',
-          fontSize: 24,
-          fontWeight: 'bold',
-          zIndex: 2,
-        }}
-      >
-        TRAINING COMPLETED
-      </Text>
-    </View>
+      <TrainingComplete>TRAINING COMPLETED</TrainingComplete>
+    </Container>
   );
 };
+
+const Container = styled.View({
+  position: 'absolute',
+  alignItems: 'center',
+  bottom: 0,
+  left: 0,
+  right: 0,
+  top: 0,
+  height: '100%',
+  width: '100%',
+});
+
+const BlackVeil = styled.View(({ theme }) => ({
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  right: 0,
+  top: 0,
+  height: '120%',
+  width: '100%',
+  backgroundColor: theme.colors.black,
+  opacity: 0.9,
+}));
+
+const TrainingComplete = styled.Text(({ theme }) => ({
+  color: theme.colors.white,
+  position: 'absolute',
+  bottom: '15%',
+  ...theme.fonts.h1,
+}));
+
+const CheckAnimation = styled(LottieView)({ opacity: 1, flex: 1 });
