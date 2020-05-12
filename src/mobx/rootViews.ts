cbec58853a55cf7982194e53d98ac1490d4c051d
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-05-12 20:43:43 +0200

    :sparkles: (Statistics) add body visualization

--- src/mobx/rootViews.ts
+++ src/mobx/rootViews.ts
@@ -1,7 +1,10 @@
 import { ModelInstanceTypeProps } from 'mobx-state-tree';
-import { isSameDay, endOfDay } from 'date-fns';
+import { isSameDay, endOfDay, isAfter } from 'date-fns';
 import _ from 'lodash';
+import { Ratios } from '@woap/components/BodyVisualisation/index';
 
+import { ExerciseSetType } from './exerciseSet/index';
+import { MuscleGroup } from './types';
 import { FinishedTrainingType } from './finishedTraining/index';
 import { RootModel } from './rootModel';
 import { TrainingType } from './training';
@@ -27,5 +30,25 @@ export const RootViews = (self: ModelInstanceTypeProps<typeof RootModel>) => ({
     return _.countBy(this.sortedFinishedTrainings, (finishedTraining: FinishedTrainingType) =>
       endOfDay(finishedTraining.date)
     );
+  },
+  getStatisticsBodyRatios(startDate: Date): Ratios {
+    const numberOfRepsForEachMuscleGroup = new Map<MuscleGroup, number>();
+    self.finishedTrainings
+      .filter((training: FinishedTrainingType) => isAfter(training.date, startDate))
+      .forEach((training: FinishedTrainingType) => {
+        training.exerciseSets.forEach((set: ExerciseSetType) => {
+          set.exercise.muscleGroups.forEach((muscleGroup: MuscleGroup) => {
+            const actualNumberOfReps = numberOfRepsForEachMuscleGroup.get(muscleGroup) || 0;
+            numberOfRepsForEachMuscleGroup.set(muscleGroup, actualNumberOfReps + set.reps);
+          });
+        });
+      });
+    const maxRepsForOneMuscleGroup = Math.max(...numberOfRepsForEachMuscleGroup.values());
+    const bodyRatios: Ratios = {};
+    for (const [muscleGroup, numberOfReps] of numberOfRepsForEachMuscleGroup.entries()) {
+      bodyRatios[muscleGroup] = numberOfReps / maxRepsForOneMuscleGroup;
+    }
+
+    return bodyRatios;
   },
 });
--- src/pages/History/HistoryStatistics/HistoryStatistics.tsx
+++ src/pages/History/HistoryStatistics/HistoryStatistics.tsx
@@ -1,8 +1,39 @@
 import React, { FunctionComponent } from 'react';
-import { NotYetImplemented } from '@woap/components/NotYetImplemented';
+import { Dimensions } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { subWeeks } from 'date-fns';
+import { observer } from 'mobx-react-lite';
+import { useStore } from '@woap/utils/hooks/useStore';
+import styled from '@woap/utils/styled-components';
+import { BodyVisualisation } from '@woap/components/BodyVisualisation';
+import { Spacer } from '@woap/components/Spacer';
 
-interface Props {}
+export const HistoryStatistics: FunctionComponent = observer(() => {
+  const { t } = useTranslation('history');
+  const store = useStore();
+  const bodyRatios = store.getStatisticsBodyRatios(subWeeks(new Date(), 1));
 
-export const HistoryStatistics: FunctionComponent<Props> = () => {
-  return <NotYetImplemented pageTitle="HistoryStatistics" />;
-};
+  return (
+    <Container>
+      <Title>{t('historyStatistics.title')}</Title>
+      <Spacer height={5} />
+      <BodyVisualisationContainer>
+        <BodyVisualisation width={Dimensions.get('window').width * 0.85} ratios={bodyRatios} />
+      </BodyVisualisationContainer>
+    </Container>
+  );
+});
+
+const Container = styled.View(({ theme }) => ({
+  padding: theme.margin.x2,
+  backgroundColor: theme.colors.background.black,
+  flex: 1,
+}));
+
+const Title = styled.Text(({ theme }) => ({
+  ...theme.fonts.h1,
+  fontWeight: 'bold',
+  color: theme.colors.white,
+}));
+
+const BodyVisualisationContainer = styled.View({ alignItems: 'center' });
