4c813caff5116f671ca6eb47b8b48c5f2e836f3a
MattAgn <matthieua@bam.tech>
2020-08-22 13:28:21 +0200

    Bump version and build

---
+++ scripts/bump.prod.sh
@@ -0,0 +1,10 @@
+## Increment BUILD number by 1
+build=$(grep -Eo 'BUILD=[0-9]*' .env.prod | grep -Eo '[0-9]+')
+incremented_build=`expr $build + 1`
+echo "build : $incremented_build"
+sed -i '' -e "s/BUILD=.*/BUILD=$incremented_build/g" .env.prod
+
+## Update version number with the one from app-version.prod
+version=$(cat app-version.prod)
+echo "version : $version"
+sed -i '' -e "s/VERSION=.*/VERSION=$version/g" .env.prod
---
+++ scripts/bump.staging.sh
@@ -0,0 +1,14 @@
+## Increment BUILD number by 1
+build=$(grep -Eo 'BUILD=[0-9]*' .env.staging | grep -Eo '[0-9]+')
+incremented_build=`expr $build + 1`
+echo "build : $incremented_build"
+sed -i '' -e "s/BUILD=.*/BUILD=$incremented_build/g" .env.staging
+
+## Update version number with the one from app-version.staging
+version=$(cat app-version.staging)
+echo "version : $version"
+sed -i '' -e "s/VERSION=.*/VERSION=$version/g" .env.staging
+
+## Git 
+git add .
+git commit -m "Bump version and build"
